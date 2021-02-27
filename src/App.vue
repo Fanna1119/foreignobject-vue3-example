eab572a41bba57d9fd04c085fb30e6671550fa9f
Stephan Langeveld <stephanlangeveld@gmail.com>
2021-02-27 18:02:58 +0200

    inital

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <foreign outsidetext="🔥prop from parent🔥">
+      <p>{{ slottext }}</p>
+    </foreign>
+  </div>
+
+  <input v-model="slottext" type="text" />
+</template>
+
+<script>
+import { ref } from "vue";
+import foreign from "./components/foreign.vue";
+
+export default {
+  name: "App",
+  components: {
+    foreign,
+  },
+
+  setup() {
+    const slottext = ref("hello");
+
+    return {
+      slottext,
+    };
+  },
+};
+</script>
---
+++ src/components/foreign.vue
@@ -0,0 +1,58 @@
+<template>
+  <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+    <polygon points="5,5 195,10 185,185 10,195" />
+    <foreignObject
+      :x="foreignobjdata.x"
+      :y="foreignobjdata.y"
+      :width="foreignobjdata.width"
+      :height="foreignobjdata.height"
+    >
+      <div xmlns="http://www.w3.org/1999/xhtml">
+        <p>{{ textobj }}</p>
+        <slot></slot>
+        <p>
+          {{ outsidetext }}
+        </p>
+      </div>
+    </foreignObject>
+    {{ foreignobjdata }}
+  </svg>
+</template>
+
+<script>
+import { computed, reactive, ref, toRefs } from "vue";
+export default {
+  props: {
+    outsidetext: {
+      type: String,
+    },
+  },
+  setup(props) {
+    const foreignobjdata = reactive({
+      x: 20,
+      z: 20,
+      width: 160,
+      height: 160,
+    });
+
+    const textobj = ref("helloworld");
+
+    const { outsidetext } = toRefs(props);
+
+    return {
+      textobj,
+      outsidetext,
+      foreignobjdata,
+    };
+  },
+};
+</script>
+
+<style scoped>
+div {
+  color: white;
+  font: 18px serif;
+  height: 100%;
+  overflow: auto;
+}
+</style> 
